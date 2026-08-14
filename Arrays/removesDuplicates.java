9c928a0d15b5880e8af2b80e7d2b47b8df6d9302
Anushka <anushkajagtap2212@gmail.com>
2026-08-14 20:13:08 +0530

    Arrays

---
+++ Arrays/removesDuplicates.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class removesDuplicates {
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length == 0) {
+            return 0;
+        }
+        int i = 0 ;
+        for(int j = 0 ; j < nums.length ; j++)
+        {
+            if(nums[j] != nums[i])
+            {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2 , 2 , 2 , 3 , 4 , 4 , 5};
+        int length = removeDuplicates(nums);
+        System.out.println("Length of array after removing duplicates: " + length);
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < length; i++) {
+            System.out.print(nums[i] + " ");
+        }   
+    }
+}
